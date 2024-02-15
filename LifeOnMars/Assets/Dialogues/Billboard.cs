c38f2fbbd2ff54c08283e768baf38cd9a210e11c
vinz321 <vincenzo.micciche.27@gmail.com>
2024-02-15 12:56:12 +0100

    Fix Grab/Locomotion

--- LifeOnMars/Assets/Dialogues/Billboard.cs
+++ LifeOnMars/Assets/Dialogues/Billboard.cs
@@ -14,9 +14,10 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation=Camera.main.transform.rotation*originalRotation;
+        //transform.rotation=Camera.main.transform.rotation*originalRotation;
 
-        //transform.rotation=Quaternion.LookRotation(-Camera.main.transform.position+transform.position, Vector3.up);
+        transform.rotation=Quaternion.LookRotation(Vector3.ProjectOnPlane(-Camera.main.transform.position+transform.position,Vector3.up), Vector3.up);
         
+        //transform.rotation=Quaternion.Euler(new Vector3(0,-Camera.main.transform.rotation.eulerAngles.y,0));
     }
 }
