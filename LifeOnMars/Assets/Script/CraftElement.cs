bd65502187515a31051d7bb5c75bd813f245fc83
Ilaria Pasini <49036293+IlariaPasini@users.noreply.github.com>
2024-02-16 16:13:30 +0100

    Prova Scriptable object

---
+++ LifeOnMars/Assets/Script/CraftElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftElement : ScriptableObject
+{
+   GameObject firstElement;
+   GameObject secondElement;
+   GameObject createdElement;
+}
