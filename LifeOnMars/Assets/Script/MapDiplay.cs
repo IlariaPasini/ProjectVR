b9d5cd1f69168fd92d4178a616a817099b4a79c4
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2024-02-16 16:12:16 +0100

    drag and drop

--- LifeOnMars/Assets/Script/MapDiplay.cs
+++ LifeOnMars/Assets/Script/MapDiplay.cs
@@ -5,22 +5,40 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MapDiplay : MonoBehaviour
 {
-    [SerializeField] private Transform pivot;
+    [SerializeField] private Transform buttonPivot;
+    [SerializeField] private Transform map;
 
-    private XRRayInteractor ray_interactor;
+    private RaycastHit? hit;                    // raycast hit struct
+    private XRRayInteractor ray_interactor;     // ray interactor
     // lanciare un raycast dal ray interactor
     // vedere se l'oggetto è la mappa
     // spostare l'oggetto sulla mano
     // al rilascio spostare l'oggetto
 
-    private void Start()
-    {
-        // get ray interactor from this hand
-        ray_interactor = GetComponent<XRRayInteractor>();
-    }
-
     public void OnSelect(SelectEnterEventArgs args)
     {
-        print("select\n");
+        // on select get current ray interactor
+        if (args.interactorObject is XRRayInteractor)
+        {
+            ray_interactor = args.interactorObject as XRRayInteractor;
+            GetComponent<MeshRenderer>().enabled = false;
+            buttonPivot.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnDeselect(SelectExitEventArgs args)
+    {
+        // try get current raycast from current ray interactor
+        ray_interactor.TryGetCurrentRaycast(out hit, out _, out _, out _, out _);
+        if (hit.HasValue)
+        {
+            buttonPivot.gameObject.SetActive(false);
+            GetComponent<MeshRenderer>().enabled = true;
+            map.position = hit.Value.point;
+            map.gameObject.SetActive(true);
+        }
+
+        // remove last ray interactor
+        ray_interactor = null;
     }
 }
