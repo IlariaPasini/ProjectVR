242592440486a21e75842c9a0e87a715c0b54bf3
vinz321 <vincenzo.micciche.27@gmail.com>
2024-02-17 00:01:21 +0100

    Antenna

--- LifeOnMars/Assets/ComponentTarget.cs
+++ LifeOnMars/Assets/ComponentTarget.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ComponentTarget : MonoBehaviour
 {
@@ -10,24 +13,59 @@ public class ComponentTarget : MonoBehaviour
     MeshRenderer mr;
     Material originalMat;
     [SerializeField]
+    bool with_object=true;
+    [SerializeField]
     GameObject requiredComponent;
     [SerializeField]
+    string expected_name;
+    [SerializeField]
     string task_to_update;
+
+    public UnityEvent onComplete;
     void Start()
     {
         mr=GetComponent<MeshRenderer>();
         originalMat=mr.material;
+        
+        Material[] newMaterials=new Material[mr.materials.Length];
 
-        mr.material=emptyMaterial;
+        for(int i=0;i<mr.materials.Length;i++){
+            newMaterials[i]=emptyMaterial;
+        }
+        mr.materials=newMaterials;
     }
-
+    
     void OnTriggerEnter(Collider other)
     {   
-        if(other.gameObject==requiredComponent){
-            other.gameObject.SetActive(false);
+        Storable s;
+        if(with_object && other.gameObject==requiredComponent) {
+            //other.gameObject.SetActive(false);
+            
+            other.GetComponent<XRGrabInteractable>().enabled=false;
 
-            mr.material=originalMat;
+            other.GetComponent<Rigidbody>().isKinematic=true;
+            other.transform.position=transform.position;
+            other.transform.rotation=transform.rotation;
+            other.transform.localScale=transform.localScale;
+            
+            onComplete.Invoke();
             TaskSystem.instance.UpdateTask(task_to_update,1);
+            gameObject.SetActive(false);
+        }
+
+        if(!with_object && other.TryGetComponent<Storable>(out s) && s.ItemName==expected_name){
+            s.enabled=false;
+            other.GetComponent<XRGrabInteractable>().enabled=false;
+
+            other.GetComponent<Rigidbody>().isKinematic=true;
+            other.transform.position=transform.position;
+            other.transform.rotation=transform.rotation;
+            other.transform.localScale=transform.localScale;
+            
+            onComplete.Invoke();
+            TaskSystem.instance.UpdateTask(task_to_update,1);
+            gameObject.SetActive(false);
+            
         }
     }
 }
