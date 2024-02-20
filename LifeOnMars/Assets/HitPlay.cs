6bef5759f3d9b80c226551c89bacb11cd27d7e8d
vinz321 <vincenzo.micciche.27@gmail.com>
2024-02-20 17:16:21 +0100

    PushDelPorcoDinci

--- LifeOnMars/Assets/HitPlay.cs
+++ LifeOnMars/Assets/HitPlay.cs
@@ -6,14 +6,24 @@ public class HitPlay : MonoBehaviour
 {
     // Start is called before the first frame update
     new AudioSource audio;
+    Vector3 startPos;
+    Quaternion startRot;
     void Start()
     {
         audio=GetComponent<AudioSource>();
+        startPos=transform.position;
+        startRot=transform.rotation;
     }
 
     // Update is called once per frame
     void OnCollisionEnter(Collision c)
     {
         audio?.Play();
+    }
+
+    public void Reset(){
+        
+        transform.position=startPos;
+        transform.rotation=startRot;
     }
 }
