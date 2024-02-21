113e689215b8c7f0d26b976eddf7bce2ff27a0d8
vinz321 <vincenzo.micciche.27@gmail.com>
2024-02-22 00:48:21 +0100

    Fix

--- LifeOnMars/Assets/DisableOnSceneChange.cs
+++ LifeOnMars/Assets/DisableOnSceneChange.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisableOnSceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
+    Scene s;
     void Start()
     {
-        
+        s=gameObject.scene;   
+        SceneManager.sceneLoaded+=DeactivateOnSceneChange;
     }
 
     // Update is called once per frame
-    void Update()
+    void DeactivateOnSceneChange(Scene s, LoadSceneMode mode)
     {
-        
+        gameObject.SetActive(this.s==s);
     }
 }
