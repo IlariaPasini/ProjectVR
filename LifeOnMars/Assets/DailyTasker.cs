ed3a533ed61c731e787cbe83719101afd0620b66
vinz321 <vincenzo.micciche.27@gmail.com>
2024-02-24 19:26:50 +0100

    CommitPericoloso

---
+++ LifeOnMars/Assets/DailyTasker.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DailyTask{
+    public List<Task> task;
+}
+
+public class DailyTasker : MonoBehaviour
+{
+
+    [SerializeField] List<DailyTask> taskPerDay;
+    void Start()
+    {
+        DaySystem.onDayChange+=OnDayChange;
+        OnDayChange(0);
+    }
+
+    void OnDestroy(){
+        DaySystem.onDayChange-=OnDayChange;
+    }
+    // Update is called once per frame
+    void OnDayChange(int day)
+    {
+        if(taskPerDay.Count>day){
+            foreach(Task t in taskPerDay[day].task){
+                TaskSystem.instance.AddTask(t.name,t);
+            }
+        }
+    }
+}
